7287d77089c5a9e95652e9c97be7b6cd5909cced
ritesh poudel <ritesh.poudel.34@gmail.com>
2024-01-04 16:54:25 +0545

    Initial commit from Create Next App

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,20 @@
-/* @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
 
-module.exports = {
+const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
- 
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        duskblue : {
-          100: '#6E8BC2',
-          200: '#5B7BB8',
-          300: '#4A6CAD',
-          400: '#395EA3',
-          500 :'#2b5199',
-          600: '#27498A',
-          700: '#22417A',
-          800: '#1E396B',
-          900: '#1A315C',
-      }
-    }
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
-    
   },
   plugins: [],
 }
+export default config
