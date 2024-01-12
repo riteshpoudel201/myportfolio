99da1d47dd30fdb11e75ac7c967397c0388034ae
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-01-12 21:37:31 +0545

    Merge branch 'test' of https://github.com/riteshpoudel201/myportfolio into test

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,23 +1,10 @@
-import Navbar from '../components/navbar';
-import Sidebar from '../components/sidebar';
 
+import Navbar from '../components/navbar';
 const Portfolio = () => {
-  
   return (
     <>
-      <div className='flex flex-col relative h-screen w-screen'>
-        <div>
-          <Navbar />
-        </div>
-        <div className='relative flex  md:flex-row md:h-[80vh] flex-col-reverse h-4 w-full'>
-          <div className=' flex-[4_4_0%] bg-white'>
-            main code here...
-          </div>
-          <div className='flex-1 z-20'>
-                    <Sidebar/>
-          </div>
-        </div>
-      </div>
+    <Navbar />
+    <div className='flex mt-10'>Portfolio page</div>
     </>
   )
 }
