01b0544c566597346d06a917a7afd308f7daa78a
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-01-15 12:27:16 +0545

    made minor changes for adjusting display correctly in mobile-like devices along with addition on circle in sidebar for medium screen and higher

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -9,11 +9,11 @@ const Portfolio = () => {
         <div>
           <Navbar />
         </div>
-        <div className='relative flex  md:flex-row md:h-[80vh] flex-col-reverse h-4 w-full'>
+        <div className='relative flex  md:flex-row md:h-[80vh] flex-col-reverse h-4 w-full border-t-[1px] border-duskblue-400 border-solid'>
           <div className=' flex-[4_4_0%] bg-white'>
             main code here...
           </div>
-          <div className='flex-1 z-20'>
+          <div className='flex-1'>
                     <Sidebar/>
           </div>
         </div>
