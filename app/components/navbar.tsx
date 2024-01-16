d455a835d393da1e5aa0f9675cfbc5ad75e9ebda
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-01-16 19:39:00 +0545

    added contact section on home page, adjusted banner for mobile devices and corrected navbar not showing properly

---
+++ app/components/navbar.tsx
@@ -0,0 +1,150 @@
+/* eslint-disable react/jsx-key */
+'use client';
+import LogoDynamic from './logo';
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaGithubSquare, FaInvision } from "react-icons/fa";;
+import { RxHamburgerMenu } from "react-icons/rx";
+import Link from '@/node_modules/next/link';
+import { usePathname } from '@/node_modules/next/navigation';
+import { useState, useEffect } from 'react';
+import DropdownMenu from './dropdown';
+
+const Navbar = () => {
+    const router = usePathname();
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const [isScroll, setScroll] = useState(false);
+
+    //array for populating navigation menu
+    const menuLink = [
+        {
+            name: "Home",
+            page: "/",
+        },
+        {
+            name: "Portfolio",
+            page: "./portfolio/",
+        },
+        {
+            name: "Contact",
+            page: "/contact",
+        }
+
+    ];
+
+    //array for populating social links
+    const socialLink = [
+        {
+            social: FaSquareFacebook,
+            link: "https://www.facebook.com/ritesh.poudel.33/",
+            color: "text-blue-800",
+        },
+        {
+            social: FaGithubSquare,
+            link: "https://github.com/riteshpoudel201",
+            color: "text-black",
+        },
+        {
+            social: FaInvision,
+            link: "https://www.linkedin.com/in/ritesh-poudel-12b950188/",
+            color: "text-blue-500",
+        }
+    ];
+
+
+
+    const handleClick = () => {
+        setMenuOpen(!isMenuOpen);
+    }
+
+    useEffect(
+        () => {
+            const handleScroll = () => {
+               setScroll(window.scrollY > 0);
+            };
+            window.addEventListener('scroll', handleScroll);
+
+            return () => window.removeEventListener('scroll',handleScroll);
+        },[]
+    );
+
+    return (
+        <>
+            <nav className={`${ isScroll ? 'fixed top-0 z-50' : '' } h-24 w-full shadow-2xl bg-white`} >
+
+                {/* navbar container starts here */}
+                <div className='flex flex-row justify-between h-full w-full items-center px-4  2xl:px-16'>
+                    {/* logo starts from here */}
+                    <div className='flex justify-center align-middle'>
+                        <Link href='/'><LogoDynamic /></Link>
+                    </div>
+                    {/* logo ends here */}
+
+                    {/* This is the nav menu start */}
+                    <div className='hidden md:flex'>
+                        <ul className='hidden md:flex flex-row gap-4'>
+                            {
+                                menuLink.map(
+                                    (items) => {
+                                        
+                                        return (
+                                            <li key={items.name} className={`${router == items.page ? 'text-duskblue-500' : 'hover:text-duskblue-300'} `}><Link href={items.page}> {items.name}</Link></li>
+                                             )
+                                        }
+                                   
+
+                                )
+                            }
+
+                        </ul>
+                    </div>
+                    {/* Nav menu ends here */}
+
+                    {/* social links starts here */}
+                    <div className='hidden md:flex flex-row gap-2 pl-3'>
+                        {
+                            socialLink.map((items) => {
+                                return (
+                                    <Link key={items.link} target="_blank" href={items.link}>< items.social size={25} className={items.color} /></Link>
+                                )
+                            })
+                        }
+
+                    </div>
+                    {/* social links end here */}
+                    <div onClick={handleClick} className="md:hidden pr-13"> <RxHamburgerMenu size={25} /></div>
+
+
+
+                </div>
+                {/* navbar container ends here */}
+
+                <div className={
+                    isMenuOpen ? "md:hidden fixe₫ right-0 top-25 w-[250px] bg-slate-500 rounded-md absolute z-50" : "fixed right-[-100%]"
+                }>
+                    <ul className='flex flex-col mt-4 p-3 '>
+                        {menuLink.map(({ page, name }) => (
+                            <li key={name} className={`${router == page ? 'bg-duskblue-500 text-white' : 'hover:text-duskblue-300'} `}><Link className='w-full' href={page}> {name}</Link></li>
+                        ))
+
+                        }
+
+                    </ul>
+
+                    <div className='flex flex-row gap-2 pl-3 mt-5 pb-2'>
+                        {
+                            socialLink.map((items) => {
+                                return (
+                                    <Link key={items.link} target="_blank" href={items.link}>< items.social size={25} className={items.color} /></Link>
+                                )
+                            })
+                        }
+
+                    </div>
+                </div>
+
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,86 @@
+import Navbar from './components/navbar';
+import Footerpage from './components/footer';
+import styles from '../app/page.module.css';
+import Link from '@/node_modules/next/link';
+// import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+
+const page = () => {
+  return (
+    <main className={styles.container}>
+
+      {/* navbar section starts here */}
+      <section className={styles.navbarContainer}>
+        <Navbar />
+      </section>
+      {/* navbar section starts here */}
+
+      {/* banner section starts */}
+      <section className={styles.bannerContainer}>
+        <div className={styles.banner}>
+          <div className={styles.contentContainer}>
+            <div className={styles.content}>
+              <span className={styles.titleText} >Ritesh Poudel</span>
+              <p className={`${styles.descText}`}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iure distinctio quas maiores, explicabo, quod commodi suscipit ab adipisci ut, vero nobis tempore molestiae modi aliquam iste tenetur molestias accusantium..
+              </p>
+            </div>
+          </div>
+          <div className={styles.boxContainer}>
+            <div className={styles.box}></div>
+          </div>
+        </div>
+      </section>
+      {/* banner section ends */}
+
+      {/* project section starts */}
+      <section className={styles.projectContainer}>
+        <div className={styles.projectHeading}>Projects</div>
+        <ul className={styles.project}>
+          <li className={styles.projectList}>
+            <ul>
+              <li className={styles.projectImage}>Image</li>
+              <li className={styles.projectTitle}>Project Title 1</li>
+              <li className={`${styles.projectDesc} ${styles.lineClamp}`}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt excepturi fuga ad sed unde illum nobis hic atque vero esse.</li>
+            </ul>
+          </li>
+          <li className={styles.projectList}>
+            <ul>
+              <li className={styles.projectImage}>Image</li>
+              <li className={styles.projectTitle}>Project Title 2</li>
+              <li className={styles.projectDesc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, optio. Nemo dolores voluptatum laborum, unde quas sint voluptatem nostrum esse, numquam illum, repellendus earum dolorem!</li>
+            </ul>
+          </li>
+          <li className={styles.projectList}>
+            <ul>
+              <li className={styles.projectImage}>Image</li>
+              <li className={styles.projectTitle}>Project Title 3</li>
+              <li className={styles.projectDesc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit architecto illo nam corrupti, aliquid in.</li>
+            </ul>
+          </li>
+          <li>
+            <div className={styles.viewMore}>
+              <Link className={styles.viewMoreButton} href="./portfolio/projects.tsx">View More</Link>
+            </div>
+          </li>
+        </ul>
+      </section>
+      {/* project section ends */}
+
+      {/* contact section starts here */}
+      <section className={styles.contactSection}>
+        <div className={styles.buttonContainer}>
+          <button className={styles.button}>Contact Me</button>
+        </div>
+      </section>
+      {/* contact section ends here */}
+
+      {/* Footer section starts*/}
+      <section className={styles.footerContainer}>
+        <Footerpage />
+      </section>
+      {/* Footer section ends */}
+    </main>
+  )
+}
+
+export default page
