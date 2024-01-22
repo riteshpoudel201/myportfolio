3c9d8ac05ce619731667d363232c5a3de766b79c
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-01-22 19:47:45 +0545

    tried to use external image but unsuccessful

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname:'**.pexels.com',
+                port:'',
+                pathname:'/photo/**'
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
