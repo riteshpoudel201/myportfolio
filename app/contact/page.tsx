1032e4d1948c7490e2e766d738ec9ea98ab27141
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-01-28 18:39:11 +0545

    delete: removed the double navbar appearing in the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import Navbar from '../components/navbar';
-
 const Contact = () => {
   return (
     <>
-    <Navbar />
+    
     <div>Contact Form</div>
     </>
   )
