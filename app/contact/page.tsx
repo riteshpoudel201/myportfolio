d84c24f94f695131c70f1f4ede01efadb4a7103f
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-02-03 17:14:37 +0545

    style: changed background color

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
           <div className="w-full h-[20%]  flex justify-center items-center">
             <h1 className="md:text-8xl text-6xl  uppercase">Contact</h1>
           </div>
-          <div className="w-full md:h-[70%] h-full flex md:flex-row flex-col justify-around mt-3 bg-gray-200 py-8 px-3 rounded-t-3xl">
+          <div className="w-full md:h-[70%] h-full flex md:flex-row flex-col justify-around mt-3 bg-slate-300 py-8 px-3 rounded-t-3xl">
               <ContactDetails />
               <ContactForm />
             
