083e9a3d49db7b4e50dfad11563cf34c55d6997b
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-02-06 19:59:21 +0545

    style: added font style and placed contact banner text at end

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,8 +7,8 @@ const Contact = () => {
     <>
       <>
         <main className="w-full h-full ">
-          <div className="w-full h-[20%]  flex justify-center items-center">
-            <h1 className="md:text-8xl text-6xl  uppercase">Contact</h1>
+          <div className="w-full h-[20vh]  flex justify-center items-end">
+            <h1 className="md:text-8xl text-6xl  uppercase font-mono">Contact</h1>
           </div>
           <div className="w-full md:h-[70%] h-full flex md:flex-row flex-col justify-around mt-3 bg-slate-300 py-8 px-3 rounded-t-3xl">
               <ContactDetails />
