4842d2cbf506c98a80cf16737a433cbe1291e6d7
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-02-06 21:43:19 +0545

    style: changed from flex row to column

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -28,9 +28,9 @@ const Details   = ( {company, jobTitle, duration, responsibilities}: any)  => {
 const Experience = () => {
   return (
     <>
-    <div id="experience" className="flex flex-row flex-wrap justify-center gap-4 mt-8 items-center px-4 overflow-y-hidden ">
+    <div id="experience" className="flex flex-col justify-between gap-4 mt-8 items-center px-4 overflow-y-hidden ">
         <h1 className="md:text-4xl text-3xl mb-4 md:no-underline underline underline-offset-8 text-duskblue-500">Experience</h1>
-        <div className="flex flex-col md:w-1/2 w-full gap-3 pl-4 md:border-l-[8px] border-0 border-solid border-black rounded-lg">
+        <div className="flex flex-col md:w-1/2 w-full gap-3 pl-4 md:border-l-[8px] border-0 border-solid border-slate-500">
           <Details company="Eastern Hawk Solutions" jobTitle="Software Developer" duration="3 months" responsibilities="Designing and building full-fledged web applications using HTML, CSS, JS and PHP."/>
           <Details company="Code Studio Solutions" jobTitle="Junior Software Developer" duration="6 months" responsibilities="Building windows applications using dot net framework in c# language"/>
           <Details company="Deerwalk Pvt. Ltd" jobTitle="Backend Software Developer" duration="1 months" responsibilities="Developing RESTful APIs for the frontend system using Node JS, Express JS and Mongo DB"/>
