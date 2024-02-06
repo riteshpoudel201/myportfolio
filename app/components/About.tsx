ab1ee090a0ada87ced37311f7ecf5e777a79ce0f
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-02-06 21:37:53 +0545

    refactor & style : refactored the page and adjusted style of about section of this page

---
+++ app/components/About.tsx
@@ -0,0 +1,37 @@
+import aboutme from "../../public/aboutme.jpg";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <div className="flex md:flex-row flex-col justify-between gap-1 items-center p-4 border-b-4">
+      <div className="flex flex-col justify-around  rounded-md p-4 md:w-1/2 w-full">
+        <h1 className="md:text-4xl text-3xl divide-y-2 divide-slate-400 md:mb-0 text-duskblue-500 ">
+          About Me
+        </h1>
+
+        <p className="h-auto md:border-l-[8px] border-solid border-slate-500 pl-2 md:mb-0 mb-4 mt-4">
+          My name is
+          <span className="font-bold">&nbsp;Ritesh Poudel</span>. I am from
+          <span className="font-bold">&nbsp;Urlabari, Morang</span>of country
+          <span className="font-bold">&nbsp;Nepal.</span>
+          &nbsp;My current phone number is
+          <span className="font-bold">&nbsp;9819347438&nbsp;</span> and my email
+          is
+          <span className="font-bold">&nbsp;ritesh.poudel.34@gmail.com</span>
+
+          
+        </p>
+      </div>
+      {/* <div className="md:w-[20vw] w-full h-full bg-slate-200 "> */}
+        <Image
+          src={aboutme}
+          className="md:w-1/4 md:h-1/2 sm:w-1/2 h-auto w-full rounded-lg"
+          alt="NO Image"
+          priority
+        />
+      {/* </div> */}
+    </div>
+  );
+};
+
+export default About;
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,8 +1,7 @@
-import aboutme from "../../public/aboutme.jpg";
-import Image from "next/image";
 import Layout from "../components/Layout";
 import Experience from "../components/Experience";
 import Projects from "../components/Projects";
+import About from "../components/About";
 
 const Portfolio = () => {
   return (
@@ -13,28 +12,7 @@ const Portfolio = () => {
 
       <main id="about" >
         <Layout className="md:p-16 p-8">
-          <div className="flex flex-row flex-wrap justify-center gap-5 items-center">
-            <h1 className="md:text-4xl text-3xl divide-y-2 divide-slate-400 md:mb-0 mb-8 text-duskblue-500 ">
-              About Me
-            </h1>
-
-            <p className="md:w-1/2 h-auto md:border-l-[8px] border-solid border-black pl-4 md:mb-0 mb-4 ">
-              My name is
-              <span className="font-bold">&nbsp;Ritesh Poudel</span>. I am from
-              <span className="font-bold">&nbsp;Urlabari, Morang</span>of
-              country
-              <span className="font-bold">&nbsp;Nepal.</span>
-              &nbsp;My current phone number is
-              <span className="font-bold">&nbsp;9819347438&nbsp;</span> and my
-              email is
-              <span className="font-bold">
-                &nbsp;ritesh.poudel.34@gmail.com
-              </span>
-            </p>
-            <div className="md:w-[20vw] w-full h-full bg-slate-200 ">
-              <Image src={aboutme} className="w-auto h-auto" alt="NO Image" priority/>
-            </div>
-          </div>
+          <About />
           <Experience />
           <Projects />
         </Layout>
